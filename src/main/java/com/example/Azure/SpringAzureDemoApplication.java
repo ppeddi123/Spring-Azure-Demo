d776d9e0f5b7b20e293a3dbd102b430775bbbaa6
Potharaju Peddi <ppeddi@nisum.com>
2023-04-10 05:59:29 +0530

    first commit

---
+++ src/main/java/com/example/Azure/SpringAzureDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.Azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAzureDemoApplication {
+
+	@GetMapping("/message")
+	public String getMessage(){
+		return "This is spring azure application0";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureDemoApplication.class, args);
+	}
+
+}
